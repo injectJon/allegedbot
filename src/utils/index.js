93f400aeb64dc7b8497cc33592a2a1db6251cc62
injectJon <injectJon@gmail.com>
2018-07-24 14:09:45 -0500

    Clean up.

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,30 +10,5 @@ export function isAdmin( message ) {
   const authorRoles = message.member.roles.array();
   const adminRole = authorRoles.filter( role => role.name === adminRoleName );
 
-  console.log( adminRole )
-
   return (adminRole.length === 1 ) ? true : false;
-}
-
-export function serverUsersList(context) {
-  // const { serverId, app } = context;
-
-  // const activeUsers = [];
-  // const myServer = app.servers.get(serverId);
-  // let slowmode = true;
-  // // console.log(myServer);
-
-  // myServer.everyServerMembers((err, user, done) => {
-  //   if (err == null && done) {
-  //     slowmode = false;
-  //     return false;
-  //   }
-  //
-  //   activeUsers.push(user._username);
-  //   return true;
-  // });
-
-  // if (slowmode === false) {
-  //   return activeUsers; // should return an array of username strings
-  // }
 }
