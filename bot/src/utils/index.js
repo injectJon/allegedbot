4a8269c2aa3019765877421d471f7485ec7164e4
injectJon <injectJon@gmail.com>
2018-07-26 18:02:21 -0500

    Convert role name to lowercase.

--- bot/src/utils/index.js
+++ bot/src/utils/index.js
@@ -4,7 +4,7 @@ export function isAdmin( message ) {
   const adminRoleName = 'AllegedBot Admin';
   const modRoleNames = 'moderator mod admin administrator '
   const authorRoles = message.member.roles.array();
-  const adminRole = authorRoles.filter( role => role.name === adminRoleName || modRoleNames.includes( role.name ) );
+  const adminRole = authorRoles.filter( role => role.name === adminRoleName || modRoleNames.includes( role.name.toLowerCase() ) );
 
   return (adminRole.length >= 1 ) ? true : false;
 }
