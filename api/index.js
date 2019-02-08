ddf72dd6d692456c8fc838d48d11474986ff977c
injectJon <injectJon@gmail.com>
2019-02-07 22:48:18 -0600

    Allow Auth on CORS

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ mongoose.connect( process.env.MONGODB_URI );
 app.use( (req, res, next) => {
   // if ( !process.env.PORT ) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   // }
 });
